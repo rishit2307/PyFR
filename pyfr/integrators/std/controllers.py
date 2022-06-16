8784cb4c9b65fcbdc9d61a09cb4e0af281a97620
Rishit Modi <rishit@spitfire.engr.tamu.edu>
2022-06-16 10:12:19 -0500

    controller of tgv_wocfl

--- pyfr/integrators/std/controllers.py
+++ pyfr/integrators/std/controllers.py
@@ -117,7 +117,7 @@ class StdPIController(BaseStdController):
         self._alpha = self.cfg.getfloat(sect, 'pi-alpha', 0.58)
         self._beta = self.cfg.getfloat(sect, 'pi-beta', 0.21)
         self._gamma = self.cfg.getfloat(sect, 'pi-beta', 0.1)
-
+      
     
        
 
@@ -194,8 +194,7 @@ class StdPIController(BaseStdController):
             # Decide on the time step
             dt = max(min(t - self.tcurr, self._dt, self.dtmax, self.cfl[-1]), self.dtmin)
             
-      
-
+           
                        
             # Take the step
             idxcurr, idxprev, idxerr = self.step(self.tcurr, dt)
