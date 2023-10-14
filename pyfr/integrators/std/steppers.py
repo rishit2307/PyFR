8bb373a554f1d90a2c9932c6697a61ebd09f60c4
Rishit Modi <rishit@tamu.edu>
2023-10-14 15:17:07 -0500

    bug fix
    bug_fix

--- pyfr/integrators/std/steppers.py
+++ pyfr/integrators/std/steppers.py
@@ -66,26 +66,24 @@ class Trapezoidal(BaseStdStepper):
         # r1 = R(Un+eps*Q)/eps + Q/dt  - R(Un)/eps 
         add(1.0, r1, 1.0/(2*eps), r2)
 
-        for etype in self.system.ele_types:
+        for etype in eletype:
             h[etype] = np.zeros(k+2)
+            qnorm[etype] = 0
         
         q = [self.system.ele_banks[i][r1].get() for i in range(netype)]
-       
+
         for j in range(0, k+1):
-            for etype in eletype:
-                idx = self.system.ele_types.index(etype)
-                h[etype][j] = np.dot(q[idx].flatten(), Q[idx][..., j].flatten())
+            for i, etype in enumerate(self.system.ele_types):
+                h[etype][j] = np.dot(q[i].flatten(), Q[i][..., j].flatten())
                 h[etype][j] = comm.allreduce(h[etype][j], op=mpi.SUM)
-                q[idx] = q[idx] - h[etype][j] * Q[idx][..., j]
+                q[i] -= h[etype][j] * Q[i][..., j]
                 
 
-        for etype in eletype:
-            idx = self.system.ele_types.index(etype)
-            qnorm[etype] = np.linalg.norm(q[idx])**2
+        for i, etype in enumerate(self.system.ele_types):
+            qnorm[etype] = (np.linalg.norm(q[i]))**2
             qnorm[etype] = np.sqrt(comm.allreduce(qnorm[etype], mpi.SUM))
 
-        for etype in eletype:
-            i = self.system.ele_types.index(etype)
+        for i, etype in enumerate(self.system.ele_types):
             h[etype][k+1] = qnorm[etype]
             q[i] /= h[etype][k+1]
 
@@ -103,14 +101,24 @@ class Trapezoidal(BaseStdStepper):
         netype = len(self.system.ele_types)
         nnorm = 1.0
         rnorm, l2u, normele = dict(), dict(), dict()
+        
         if rank == 0:
             eletype = self.system.ele_types
         else:
             eletype = None
 
         eletype = comm.bcast(eletype, root=root)
+        for etp in eletype:
+            rnorm[etp] = 0
+            l2u[etp] = 0
+            normele[etp] = 0
+
 
         nonlin_iter = 0
+        lconv = [[] for _ in range(netype)]
+        lintol = 1e-4
+
+
         while np.amax(nnorm) > ntol:
             e1 = [np.zeros(m+1) for i in range(netype)]
             for i in range(netype):
@@ -120,11 +128,11 @@ class Trapezoidal(BaseStdStepper):
 
             x = [self.system.ele_banks[i][r2].get() for i in range(netype)]
 
-            for etype in eletype:
-                i = self.system.ele_types.index(etype)
+            for i, etype in enumerate(self.system.ele_types):
                 l2u[etype] = (np.linalg.norm(self.soln[i]))**2
+
+            for etype in eletype:
                 l2u[etype] = np.sqrt(comm.allreduce(l2u[etype], op=mpi.SUM))
-            
             
             max_etp = max(l2u, key=l2u.get)
             
@@ -146,18 +154,22 @@ class Trapezoidal(BaseStdStepper):
 
             # r1 = b - Adu0: first step of GMRES
             add(-1.0, r1, 1.0, r2)
-
+            
+            for i, etype in enumerate(self.system.ele_types):
+                rnorm[etype] = (np.linalg.norm(self.system.ele_banks[i][r1].get()))**2
+            
             for etype in eletype:
-                idx = self.system.ele_types.index(etype)
-                rnorm[etype] = (np.linalg.norm(self.system.ele_banks[idx][r1].get()))**2
                 rnorm[etype] = np.sqrt(comm.allreduce(rnorm[etype], op=mpi.SUM))
 
+            print(rnorm)
+            exit()
             Q = [[] for i in range(netype)]
             
             for i, etype in enumerate(self.system.ele_types):
                 nupts, nvars, neles = self.system.ele_shapes[i]
                 Q[i] = np.zeros((nupts, nvars, neles, m+1))
                 Q[i][..., 0] = self.system.ele_banks[i][r1].get() / rnorm[etype]
+            
             
             H = [np.zeros((m+1, m)) for i in range(netype)]
 
@@ -166,11 +178,17 @@ class Trapezoidal(BaseStdStepper):
             
             for k in range(m):
                 h, q = self.arnoldi(Q, k, eps, dt, t, eletype)
+                
                 for i, etype in enumerate(self.system.ele_types):
                     H[i][:k+2, k], Q[i][..., k+1] = h[etype], q[i]
-
+                    
+                    
+                
+            
+            
             for i, etype in enumerate(self.system.ele_types):
-                y[i] = np.linalg.lstsq(H[i][:k+1, :k+1], beta[i][:k+1], rcond=None)[0]
+                y[i], _, _, lconv[i] = np.linalg.lstsq(H[i][:k+1, :k+1], 
+                                        beta[i][:k+1], rcond=None)
                 x[i] += Q[i][..., :k+1] @ y[i]
                 self.system.ele_banks[i][r2].set(x[i])
             
@@ -202,7 +220,10 @@ class Trapezoidal(BaseStdStepper):
             nonlin_iter+= 1
             if rank == root:
                 print(nnorm)
+            
+        
                 print(nonlin_iter)
+            exit()
        
         return r0
 
